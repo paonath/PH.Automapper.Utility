0e74c5df5512475cd307fcdbca644a5957660f1a
Paolo Innocenti <paonath@gmail.com>
2021-09-28 11:49:13 +0200

    WIP: project stub

---
+++ src/PH.Automapper.Utility.DtoGenerator.Lib/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PH.Automapper.Utility.DtoGenerator.Lib
+{
+    public enum CustomModifier
+    {
+        Public = 0,
+        Internal = 1,
+        Protected = 2
+    }
+
+    public interface IDtoGeneratorUtility
+    {
+        string GenerateDto(Type sourceType, string dtoNameSpace, CustomModifier modifier = CustomModifier.Public);
+    }
+
+    public class DtoGeneratorUtility : IDtoGeneratorUtility
+    {
+        public string GenerateDto(Type sourceType, string dtoNameSpace, CustomModifier modifier = CustomModifier.Public)
+        {
+            
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ src/PH.Automapper.Utility.DtoGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PH.Automapper.Utility.DtoGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
